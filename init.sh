8a550cf3a625ebd2ff03a7e9061ede0750e43e1a
itfront <itfront@163.com>
2018-07-05 11:49:58 +0800

    vue integrated init

--- init.sh
+++ init.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-cordova create smartpos com.plutuspay.smartpos SmartPos
+vue create smartpos   # create your vue project. install @vue/cli if you haven't.
 cd smartpos
-cordova platform add android
+vue add cordova         # add vue-cli-plugin-cordova and invoke it
+
 cordova plugin add cordova-plugin-code-push
 cordova plugin add cordova-plugin-advanced-http
 cordova plugin add cordova-plugin-geolocation
 cordova plugin add cordova-sqlcipher-adapter
 cordova plugin add cordova-plugin-app-launcher
 cordova plugin add cordova-plugin-qrscanner
+
+yarn run cordova-build   # make sure you can build your project
+cordova prepare
