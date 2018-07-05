09e90d3c48a526f465d77f988da934134e23c91c
itfront <itfront@163.com>
2018-07-05 11:35:03 +0800

    vue integrated

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import 'typeface-roboto';
+import 'material-design-icons/iconfont/material-icons.css';
+import 'vuetify/dist/vuetify.css';
+import 'babel-polyfill';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import cordovaLoader from './cordovaLoader';
+import vuetify from 'vuetify';
+Vue.config.productionTip = false;
+Vue.use(vuetify);
+cordovaLoader(() => {
+    new Vue({
+        router,
+        store,
+        render: (h) => h(App),
+    }).$mount('#app');
+});
+//# sourceMappingURL=main.js.map
